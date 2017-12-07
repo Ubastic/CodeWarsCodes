0097cec84526a3614c037525632935154cdcd4db
sangbpark94 <sangbp1994@gmail.com>
2017-12-06 20:10:03 -0500

    made progress on infinite digital string problem

--- GettingAlongWithIntegerPartitions/src/Main.java
+++ GettingAlongWithIntegerPartitions/src/Main.java
@@ -2,7 +2,19 @@
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println(IntPart.part(43));
+		
+		System.out.println(IntPart.part(10));
+		System.out.println(IntPart.part(20));
+		System.out.println(IntPart.part(30));
+		System.out.println(IntPart.part(40));
+		System.out.println(IntPart.part(50));
+		/*
+		System.out.println(test.part(10));
+		System.out.println(test.part(20));
+		System.out.println(test.part(30));
+		System.out.println(test.part(40));
+		System.out.println(test.part(50));
+		*/
 	}
 	
 }
