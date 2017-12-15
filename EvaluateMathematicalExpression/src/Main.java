e9906262fdc40e3e21b91df6ffc04644a04619e0
sangbpark94 <sangbp1994@gmail.com>
2017-12-15 17:16:24 -0500

    mathematical expression problem passes sample tests

---
+++ EvaluateMathematicalExpression/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		System.out.println(MathEvaluator.calculate("-123"));
+		//System.out.println(MathEvaluator.evaluate("2 + 3"));
+	}
+
+}
---
+++ RegularExpressionParser/src/RegExpParser.java
@@ -0,0 +1,14 @@
+
+public class RegExpParser {
+
+	public RegExpParser(final String input) {
+		
+	}
+
+	public RegExp parse() {
+		
+		return new Void();
+	
+	}
+	
+}
